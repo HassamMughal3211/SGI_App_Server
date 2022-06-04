e676c5b4cd0a20e26e8276f1f240603be749f8d0
Zaid Ahmed <ahmedsyedzaid@gmail.com>
2022-06-04 18:15:26 +0500

    update

--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { signup, fetchUser, signin } = require("../controllers/authController");
 const { paymentIntent } = require("../controllers/stripeController");
 
 
